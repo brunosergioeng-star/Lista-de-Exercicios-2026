b24e053f027aadc34b8b0cc657ca073239e7a392
Bruno Sergio Oliveira Muiniz <brunosergio.eng@gmail.com>
2026-04-02 20:51:52 -0300

    Finalizado exercicio 04

---
+++ Exercicio04.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿/*4. Crie um programa para calcular o salário total de um vendedor. Deverá ser informado o salário
+base e o total de vendas. A comissão é calculada com um percentual (informado pelo
+usuário) sobre o total de vendas.
+*/
+
+Console.WriteLine("Informe o valor do salario: ");
+double salario = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Informe o valor da comissao: ");
+double comissao = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Informe a porcentagem para comições: ");
+double porcentagem = Convert.ToDouble(Console.ReadLine());
+double comissaoPorcentagem = comissao * (porcentagem / 100);
+
+double salarioComissao = salario + comissaoPorcentagem;
+Console.WriteLine($"O valor do salario mensal com comissão é: {salarioComissao}");
+Console.ReadLine();
