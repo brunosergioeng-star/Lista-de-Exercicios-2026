973effdc78af513eaa0debca0669768ab9e05698
Bruno Sergio Oliveira Muiniz <brunosergio.eng@gmail.com>
2026-04-02 20:20:35 -0300

    Adicionado exercicio 03

--- Exercicio02.ConsoleApp/Program.cs
+++ Exercicio02.ConsoleApp/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿/*2. Crie um programa que calcule o consumo de combustível por quilômetro percorrido em uma
+viagem.
+O programa deve solicitar ao usuário:
+a. A quilometragem inicial do veículo no início da viagem.
+b. A quilometragem final ao término da viagem.
+c. A quantidade de combustível consumida durante a viagem (em litros).
+*/
+
+Console.WriteLine("Digite a quilometragem informada no painel do veiculo, no incio da viagem");
+double kmInicial = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite a quilometragem informada no painel do veiculo, no final da viagem");
+double kmFinal = Convert.ToDouble(Console.ReadLine());
+
+double deslocamento = kmInicial - kmFinal;
+
+Console.WriteLine("A distância percorrida durante a viagem foi: " + deslocamento);
+
+Console.WriteLine("Informe o volume do tanque ou quantos litros foram abastecidos");
+double tanque = Convert.ToDouble(Console.ReadLine());
+
+double consumoMedio = deslocamento / tanque;
+
+Console.WriteLine($"O consume médio na viagem foi: {consumoMedio} Km/L");
+Console.ReadLine();
