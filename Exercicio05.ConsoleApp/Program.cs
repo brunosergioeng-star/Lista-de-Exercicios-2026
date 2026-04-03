041e0133c76ecd86da316f7c2ae1f985e8ab46a8
Bruno Sergio Oliveira Muiniz <brunosergio.eng@gmail.com>
2026-04-02 21:18:22 -0300

    Inicio Logica Exercicio 5

---
+++ Exercicio05.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+5. Crie um programa para calcular a média ponderada de duas provas realizadas por um aluno
+*/
+
+Console.WriteLine("Digite o  peso primeira nota: ");
+Double pesoPrimeiraNota = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o valor da primeira nota: ");
+Double primeiraNota = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o peso da segunda nota: ");
+Double pesoSegundaNota = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o valor da segunda nota: ");
+Double SegundaNota = Convert.ToDouble(Console.ReadLine());
+
+Double Media = (pesoPrimeiraNota * primeiraNota) + (pesoSegundaNota * SegundaNota) / (primeiraNota + SegundaNota);
+Console.WriteLine($"A média ponderada do aluno é: {Media}");
+
+Console.ReadLine();
