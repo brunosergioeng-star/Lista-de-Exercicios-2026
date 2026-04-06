a796e5437d62c3e391e9eb897ee6f58a0d8fbe55
Bruno Sergio Oliveira Muiniz <brunosergio.eng@gmail.com>
2026-04-06 19:41:03 -0300

    Exercicio 06 concluido

---
+++ Exercicio06.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿/*6. Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor
+que C.
+*/
+
+Console.WriteLine("Digite o valor para A");
+Double valorA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o valor para B");
+Double valorB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o valor para C");
+Double valorC = Convert.ToDouble(Console.ReadLine());
+
+Double soma = valorA + valorB;
+
+if (valorA + valorB < 1 valorC)
+{
+    Console.WriteLine("A soma de A + B é menor que C: " + soma);
+}
+else
+{
+    Console.WriteLine("A soma de A + B não é menor que C!");
+}
+
+Console.ReadLine();
