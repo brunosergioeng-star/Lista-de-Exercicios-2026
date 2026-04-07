351a956be821ab6c995fc7c6f131b8218a4db497
Bruno Sergio Oliveira Muiniz <brunosergio.eng@gmail.com>
2026-04-07 20:28:08 -0300

    Concluido Exercicios 7 9 10

--- Exercicio07.ConsoleApp/Program.cs
+++ Exercicio07.ConsoleApp/Program.cs
@@ -11,4 +11,33 @@ f. Acima do peso
 g. Acima de 30 obeso
 */
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Digite seu peso em Kg:");
+decimal peso = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite sua altura em metros:");
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+decimal imc = peso / (altura * altura);
+Console.WriteLine("Seu IMC é: " + imc);
+
+if (imc < 18.5m)
+{
+    Console.WriteLine("Abaixo do peso");
+}
+if (imc < 18.5m)
+
+{
+    Console.WriteLine("Abaixo do peso");
+}
+
+else if (imc < 25)
+
+{
+    Console.WriteLine("Peso normal");
+}
+
+else if (imc < 30)
+{
+    Console.WriteLine("Acima do peso");
+}
+
---
+++ Exercicio09.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿/* 9. Escreva um algoritmo que leia um valor inicial A e imprima a sequência de valores do cálculo
+de A! e o seu resultado.
+a. Ex: 5! = 5 X 4 X 3 X 2 X 1 = 120
+b. Pesquise sobre “fatorial”
+*/
+
+int A, fatorial = 1;
+
+Console.WriteLine("Digite um valor para A:  ");
+A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(A + "! = ");
+
+for (int i = A; i >= 1; i--)
+{
+    Console.Write(i + " ");
+    fatorial *= i;
+
+    if (i > 1)
+    {
+        Console.WriteLine("X ");
+    }
+}
+
+Console.Write("= " + fatorial);
+
+Console.ReadLine();
